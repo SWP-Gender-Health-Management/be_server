403cec29351e8243ceb337825684d01695e74691
HarryDo29 <anhdonguyennhi@gmail.com>
2025-05-22 01:21:23 +0700

    1 cai gi do cung khong bic nua

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,21 +1,21 @@
-import 'reflect-metadata';
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/Entity/User.entity';
+import 'reflect-metadata'
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import { User } from '../models/Entity/User.entity'
 
 export class UserController {
   async getAll(req: Request, res: Response) {
-    const userRepository = getRepository(User);
-    const users = await userRepository.find();
-    res.json(users);
+    const userRepository = getRepository(User)
+    const users = await userRepository.find()
+    res.json(users)
   }
 
   async create(req: Request, res: Response) {
-    const userRepository = getRepository(User);
-    console.log(req);
-    const { name, email } = req.body;
-    const userTmp = userRepository.create({ name, email });
-    await userRepository.save(userTmp);
-    res.status(201).json(userTmp);
+    const userRepository = getRepository(User)
+    console.log(req)
+    const { name, email } = req.body
+    const userTmp = userRepository.create({ name, email })
+    await userRepository.save(userTmp)
+    res.status(201).json(userTmp)
   }
 }
