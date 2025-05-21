b3cb43022e0b327388f946ed56f26f7f7c9d34d2
DuyPham <phamquocduy107@gmail.com>
2025-05-22 00:35:16 +0700

    create file constants/idPrefix

--- src/models/Entity/User.entity.ts
+++ src/models/Entity/User.entity.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { Entity, Column, PrimaryGeneratedColumn, PrimaryColumn, BeforeInsert } from 'typeorm';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidvg4 } from 'uuid';
 import idPrefix from '~/constants/idPrefix';
 
 @Entity()
@@ -17,7 +17,7 @@ export class User {
   @BeforeInsert()
   generateId() {
     const prefix = idPrefix.CUSTOMER;
-    const uuidPart = uuidv4().split('-')[0]; // Lấy 8 ký tự đầu của UUID
+    const uuidPart = uuidvg4().split('-')[0]; // Lấy 8 ký tự đầu của UUID
     const numericId = parseInt(uuidPart, 16).toString().padStart(8, '0'); // Chuyển hex sang số và đảm bảo 8 chữ số
     this.id = `${prefix}-${numericId}`;
   }
